5306082108300c618178756d8eac3977f836a0c9
Robert Patrona <robert@iviteb.com>
2024-03-07 18:03:27 +0200

    testing ssr

--- react/components/Suggestions/index.tsx
+++ react/components/Suggestions/index.tsx
@@ -25,47 +25,57 @@ const Suggestions: FC<SuggestionsProps> = ({ customPage }) => {
     },
   } = useSearchPage()
 
-  if (!fullText) {
-    return null
-  }
+  console.log("🚀 ~ fullText:", fullText)
 
-  const { loading, data } = useQuery(searchSuggestionsQuery, {
+
+  // if (!fullText) {
+  //   return null
+  // }
+
+  const { data } = useQuery(searchSuggestionsQuery, {
     variables: {
-      fullText,
+      fullText: fullText,
     },
+    skip: !fullText,
+    ssr: true
   })
+
+  console.log("🚀 ~ data:", data)
 
   const suggestion: Suggestion | undefined = data?.searchSuggestions
 
-  if (loading || !suggestion || suggestion.searches.length === 0) {
-    return null
-  }
+  // if (loading || !suggestion || suggestion.searches.length === 0) {
+  //   return null
+  // }
 
   return (
-    <div
-      className={`${styles.suggestionListWrapper} flex flex-row items-center`}
-    >
-      <p className={`${styles.suggestionsListPrefix} mr5 b f7 ma0`}>
-        <FormattedMessage id="store/searchSuggestions" />
-        {': '}
-      </p>
-      <ul className={`${styles.suggestionsList} list flex pa0 ma0`}>
-        {suggestion.searches.map(search => (
-          <li className={styles.suggestionsListItem} key={search.term}>
-            <Link
-              className={`${styles.suggestionsListLink} link f7`}
-              page={customPage || 'store.search'}
-              query="map=ft"
-              params={{
-                term: search.term,
-              }}
-            >
-              {search.term}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div
+        className={`${styles.suggestionListWrapper} flex flex-row items-center`}
+      >
+        <p className={`${styles.suggestionsListPrefix} mr5 b f7 ma0`}>
+          <FormattedMessage id="store/searchSuggestions" />
+          {JSON.stringify(suggestion ?? {})}
+          {': '}
+        </p>
+        <ul className={`${styles.suggestionsList} list flex pa0 ma0`}>
+          {suggestion?.searches?.map(search => (
+            <li className={styles.suggestionsListItem} key={search.term}>
+              <Link
+                className={`${styles.suggestionsListLink} link f7`}
+                page={customPage || 'store.search'}
+                query="map=ft"
+                params={{
+                  term: search.term,
+                }}
+              >
+                {search.term}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   )
 }
 
